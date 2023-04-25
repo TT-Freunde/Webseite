fc8779b1f9b2a5c28ea65ca107869a3c62878630
TT-Freunde <tt.poppenhausen@outlook.de>
2023-04-26 00:28:42 +0200

    update_26.04.2023
    Aktuelles & unsere Motivation

--- src/components/Aktuelles.vue
+++ src/components/Aktuelles.vue
@@ -2,10 +2,13 @@
 <div>
     <h3>Aktuelles:</h3>
     <h4>- Werntalhalle Poppenhausen "Tag der offenen Tür": Am Samstag, den 29.04.2023 zwischen 14:00- 16:00 Uhr besteht die Möglichkeit die Tischtennisabteilung kennen zu lernen und TT zu spielen.</h4>
+    <h4>- TT-Mini-Meisterschaft mit ca. 30-40 Kids aus dem BTTV Bezirk-Nord am 13.05.2023 zwischen 11:00-14:00 Uhr in der Werntalhalle.</h4>
     <h4>- Aktuell finden Planungen statt, eine TT-Jugendmannschaft für die BTTV Herbstsaison 2023 zu melden.</h4>
     <h4>- Wir suchen einen guten TT-Spieler/-in mit BTTV-Spielbetrieb-Erfahrung, welche/-r sich als Jugendtrainer/-in engagiert und unsere 8-12 Kinder/ Jugendliche auf das nächste Level hebt. Er wird unterstützt durch den TT-Spartenleiter und 2 Senioren aus der Erwachsengruppe.</h4>
     <h4>- Infos zum Vereinsrecht:</h4>
     <a href="https://www.bmj.de/SharedDocs/Publikationen/DE/Leitfaden_Vereinsrecht.html">Bundesministerium der Justiz: Leitfaden Vereinsrecht</a>
+    <br>
+    <a href="https://auren.com/de/blog/verein-ohne-vorstand-wann-wird-das-fuer-den-verein-und-den-alt-vorstand-zum-problem/">Rechteberatung Auren: Verein ohne Vorstand</a>
     <br>
     <br>
     <div class="row">
