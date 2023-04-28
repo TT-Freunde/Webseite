1266f7169ecebbfb71b8f99b53e52a38585097bf
TT-Freunde <tt.poppenhausen@outlook.de>
2023-04-29 01:42:05 +0200

    29042023_V2
    Impressum

--- src/views/Impressum.vue
+++ src/views/Impressum.vue
@@ -5,11 +5,12 @@
         <h1>Impressum:</h1>
         <h5>
         Angaben gemäß § 5 TMG
-        <br><br>Alexander Ortloff
+        <br><br>TSV Poppenhausen 1910 e.V.
         <br>Rudolf-Werner-Straße 1A
         <br>97490 Poppenhausen</h5>
-        <br><h5><b>Vertreten durch:</b>
-        <br>Alexander Ortloff</h5>
+        <br><h5><b>Beauftragter für diese Hompage:</b>
+        <br>Alexander Ortloff
+        <br>@: tt-poppenhausen@outlook.de </h5>
         <br><h5><b>Haftung für Inhalte</b>
         <br> Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen. 
         <br><b>Bildrechte</b>
