76b1617dfecb6a570b88309470108c3666614e80
TT-Freunde <tt.poppenhausen@outlook.de>
2024-01-13 13:24:53 +0100

    update_13012024

--- src/views/TSV.vue
+++ src/views/TSV.vue
@@ -16,14 +16,14 @@
                 <h5>TSV Poppenhausen II: Milas, Samy, Jaron, Paul G., Leo G., Leo R., Mathis, Lukas, Vitali, Alex.</h5> 
                 <h5>Link zum Spielplan unserer 2. Jugendmannschaft in 2024 <a href="https://www.mytischtennis.de/clicktt/ByTTV/JRR-23-24/jugend-punktspiele-rr/Bezirksklasse-C-Winter-Gruppe-1-West/gruppe/462562/tabelle/gesamt/" target=”_blank”> BTTV Bezirksklasse-C-Winter-Gruppe-1-West </a> </h5>
                 <br>
-                <h5>Jeder Spieler der 2. Mannschaft darf in der 1. Mannschaft eingesetzt werden</h5>
+                <h5>Jeder Spieler der 2. Mannschaft darf in der 1. Mannschaft eingesetzt werden.</h5>
                 <h5> <a href="https://de.wikipedia.org/wiki/Tischtennis-Rating" target=”_blank”> Was ist der QTTR Wert? (Siehe Wikipedia) </a></h5>
                 <br>
                 <h5>Alle Kids, welche nicht in der BTTV Mannschaft sind, bzw. im BTTV-Tischtennis-Ranking auf etwa 800 TTR-Punkte kommen, können an den Bezirks-Kids-Open (kurz BKO, bzw. Turniere für TT-Einsteiger) teilnehmen. Diese finden in der Regel 1x im Quartal statt.</h5>
                 <br>                
               <hr>
               <br>
-            <h3> Termine in 2024</h3>   
+            <h3>Termine in 2024 (Teilnahme ist freiwillig)</h3>   
              <br>
              <h5> - So, 4. Feb. 10:00   - BezRLT (24-25) Jugend 19-15-13 Unterfranken-Nord Marienbachzentrum Dittelbrunn </h5>
              <h5> - Fr-So, 08.-10. März - Mini/ Kids Open Turniere im Bezirk Nord, Mitte, Süd</h5>
@@ -33,6 +33,7 @@
              <h5> - Fr, 19. Apr. 18:30  - Bezirksjugendforum Jugendleiter und Trainerinnen Unterfranken-Nord TSV-Sportheim Poppenhausen</h5>
              <h5> - Sa/So, 04/05. Mai   - TT-Co-Trainerausbildung in Würzburg - Teil 1</h5>
              <h5> - Sa, 11. Mai. 11:00  - BKO (Bezirk-Kids-Open) Jg. 2011 und jünger o. TTR bis 800 Unterfranken-Nord Schulturnhalle Schwanfeld</h5>
+             <h5> - Do-So, 13.-16. Juni - <a href="https://www.tt-finals.de/" target=”_blank”> TT Finals (Deutsch Meisterschaft), Erfurt  </a></h5>
              <h5> - So, 14. Jul. 11:00  - BKO (Bezirk-Kids-Open) Jg. 2011 und jünger o. TTR bis 800 Unterfranken-Nord Hugo-von-Trimberg-Halle Ndw. </h5>
              <h5> - Herbst 2024,        - BKO (Bezirk-Kids-Open) Jg. 2011 und jünger o. TTR bis 800 Unterfranken-Nord, Poppenhausen</h5>
             <hr>
@@ -56,7 +57,7 @@
                        <h3> Alle Termine des TT-Bezirks Ufr Nord in 2024</h3>  
                         <br> 
                     <iframe id="spielplan" src="https://www.bttv.de/fileadmin/bttv/media/b02/Termine/BTTV-UfrN__I__231101_-ST__Terminplan_.pdf" title="BTTV Portal- Bezirk Ufr Nord"></iframe>
-                    </div>
+                    </div>.
                     <div class="col-1"/>
                     </div>
                     <br>
