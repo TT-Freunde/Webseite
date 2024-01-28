1de7fd44408cf65f4ee193bb53996bb6acfe684f
TT-Freunde <tt.poppenhausen@outlook.de>
2024-01-28 22:48:02 +0100

    update_28012024_2

--- src/components/TSVGruss.vue
+++ src/components/TSVGruss.vue
@@ -7,7 +7,7 @@
             <div class="col-10 d-flex justify-content-between">
                 <h5 class="align-self-center">Mit freundlicher Unterstützung des TSV Poppenhausen 1910 e.V. </h5>
                 
-                <img class="align-self-center" src="../assets/Bilder/TSVPoppenhausenLogo.jpeg">
+                <img class="align-self-center" src="../assets/Bilder/TSVWappenkd.jpg">
                 <a class="align-self-center" href="https://tsv-poppenhausen-ufr.de"><h5>Webseite: TSV Poppenhausen 1910 e.V. </h5></a>
             </div>
             <div class="col-1"/>
